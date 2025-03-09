2635395d9fef6a87a1523972eaf9d41737f9b1bc
YoussefElGharbaouiDevs <youssefelgharbaoui.pro@gmail.com>
2025-03-09 03:17:56 +0000

    Create models and ef migration

---
+++ ExpenseTracker/ExpenseTracker.infrastructure/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.infrastructure.Common;
+
+public class BaseEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public string CreatedBy { get; set; }
+
+    public string ModifiedBy { get; set; }
+}
