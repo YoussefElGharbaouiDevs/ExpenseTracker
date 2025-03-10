573f9b91c6d553a903171d936217336bf374fe08
YoussefElGharbaouiDevs <youssefelgharbaoui.pro@gmail.com>
2025-03-10 22:45:21 +0000

    Delete obj

--- ExpenseTracker/ExpenseTracker.infrastructure/obj/Debug/net8.0/ExpenseTracker.infrastructure.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker.infrastructure/obj/Debug/net8.0/ExpenseTracker.infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f399391360e6bf9325c615759e1b5b8c32048f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
